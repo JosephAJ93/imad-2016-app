807416a94081466ba313c6a2916025d22e329c6c
JosephAJ93 <ajmaxie2k11@gmail.com>
2016-10-11 14:26:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,10 @@
-console.log('Loaded!');
-
-
+var button=document.getElementById('mainbutton');
+var counter = 0;
+button.onclick = function () {
+    
+    
+    counter = counter + 1;
+    var div=document.getElementById('span');
+    div.innerHTML = counter.toString();
+    
+};
